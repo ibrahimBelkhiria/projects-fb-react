5fa4f509d959e0caf2d33b46d9b092257b307986
ibrahim <ibrahim.belkhiria@episousse.com.tn>
2018-11-25 11:18:37 +0000

    hiding fbconfig

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,12 +4,12 @@ import 'firebase/firestore'
 
 
 var config = {
-    apiKey: "AIzaSyCpKiwf3pW0vMhAw4ll12gzuRtutzA6-ik",
-    authDomain: "project-management-36a02.firebaseapp.com",
-    databaseURL: "https://project-management-36a02.firebaseio.com",
-    projectId: "project-management-36a02",
-    storageBucket: "project-management-36a02.appspot.com",
-    messagingSenderId: "32124845550"
+    apiKey: "**********",
+    authDomain: "*********************",
+    databaseURL: "*********************",
+    projectId: "***************",
+    storageBucket: "******************",
+    messagingSenderId: "**************"
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({timestampsInSnapshots:true});
